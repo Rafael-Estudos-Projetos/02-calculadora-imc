40fa515f04e48e63f9e516d979ced475aac58a1c
rafmob <89050871+rafmob@users.noreply.github.com>
2022-11-17 20:55:52 -0300

    funcoes do modal e validação

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
+import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+
 // VARIÁVEIS
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
 
 form.onsubmit = function (event) {
   event.preventDefault()
@@ -13,15 +12,24 @@ form.onsubmit = function (event) {
   const weight = inputWeight.value
   const height = inputHeight.value
 
+  const showAlertError = notANumber(weight) || notANumber(height)
+
+  if (showAlertError) {
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  Modal.message.innerText = message
+  Modal.open()
 }
 
-modalBtnClose.onclick = () => {
-  modalWrapper.classList.remove('open')
+function notANumber(value) {
+  return isNaN(value) || value == ""
 }
 
 function IMC(weight, height) {
